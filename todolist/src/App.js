0168aab2f158fe00c4c15b4faeb225cbf998db15
Accel <cyberpunk2076@gmail.com>
2020-09-02 12:32:27 +0700

    bai 6 Component

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import LoginForm from './components/LoginForm';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
-  const list = [
-      'Go to market',
-      'Buy food',
-      'Make dinner'
-  ];
-  const listMap = list.map(x => <li>{x}</li>);
-  return (<ul>{listMap}</ul>);
+  // const list = [
+  //     'Go to market',
+  //     'Buy food',
+  //     'Make dinner'
+  // ];
+  // const listMap = list.map(x => <li>{x}</li>);
+  //<ul>{listMap}</ul>
+  return (
+      [<Header/>,
+      <LoginForm/>]
+  );
 }
 
 export default App;
